3bf152529824c665dd8c0bb895abcfc708546d3d
Cignaci0 <132091498+Cignaci0@users.noreply.github.com>
2025-11-19 18:27:00 -0300

    Refactor predictor.py for improved readability

--- predictor.py
+++ predictor.py
@@ -25,7 +25,7 @@ def predecir_vida_util(ph, tds):
     # Normalizar (Si los valores son válidos)
     ph_norm = (ph - 6.5) / (8.5 - 6.5)
     tds_norm = (tds - 50) / (500 - 50)
-
+    # introducir valor en una numpy array 
     ph_input = np.array([[ph_norm]])
     tds_input = np.array([[tds_norm]])
 
@@ -42,34 +42,28 @@ def predecir_vida_util(ph, tds):
     
     # Devolver el valor
     return max(0, dias_restantes) 
-
+# End point para mandar la respuesta hacia la app
 @app.route("/predict", methods=["POST"])
 def predic():
-
     if not modelo_ph or not modelo_tds:
         logging.error("Llamada a /predict pero los modelos no están cargados.")
         return jsonify({"error": "Los modelos no están cargados"}), 500
-    
     try:
         data = request.json
         ph = float(data['ph'])
         tds = float(data['tds'])
-        
         logging.info(f"Petición de predicción recibida: pH={ph}, TDS={tds}")
-        
         dias_restantes = predecir_vida_util(ph, tds) 
-        
         logging.info(f"Predicción: {dias_restantes:.2f} días")
-        
         return jsonify({
             "dias_restantes": f"{dias_restantes:.2f}"
         })
     except Exception as e:
         logging.error(f"Error en la predicción: {e}", exc_info=True)
         return jsonify({"error": str(e)}), 400
-    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
+
 
 
 
